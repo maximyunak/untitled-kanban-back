e93b9e762b10eb9fde18e2e523220f5a651e7016
maximyunak <kleok0039@gmail.com>
2025-11-13 15:44:09 +0300

    feat: регистрацию сделал

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -3,17 +3,23 @@
 namespace App\Http\Controllers\Auth;
 
 use App\DTOs\Auth\RegisterDTO;
+use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\RegisterRequest;
 use App\Models\User;
 use App\Services\AuthService\AuthService;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Hash;
 
-class AuthController
+class AuthController extends Controller
 {
     public function __construct(private readonly AuthService $authService) {}
 
-    public function register(RegisterRequest $request)
+    public function register(RegisterRequest $request): JsonResponse
     {
+        $dto = new RegisterDto(...$request->validated());
 
+        $user = $this->authService->register($dto);
+
+        return $this->success(code: 201, message: "Registration successful", data: $user);
     }
 }

97eaf902a2ddd6408664a3853999e2493b4deff7
maximyunak <kleok0039@gmail.com>
2025-11-14 09:42:55 +0300

    feat: middleware для проверки токена

---
+++ app/Http/Middleware/CheckToken.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class CheckToken
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $access_token = $request->cookie("access_token");
+
+        try {
+            $user = JWTAuth::setToken($access_token)->authenticate();
+            if (!$user) {
+                return throw new UnauthorizedHttpException("Invalid token");
+            }
+        } catch (JWTException $e) {
+            return throw new UnauthorizedHttpException("Invalid token");
+        }
+
+        auth()->login($user);
+
+        return $next($request);
+    }
+}

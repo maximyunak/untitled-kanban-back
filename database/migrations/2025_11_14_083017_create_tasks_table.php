6a6c10287754e4830567d28cd6729014046e8ecd
maximyunak <kleok0039@gmail.com>
2025-11-15 15:37:44 +0300

    feat: сделал модели для доски(columns, tasks, boards)

---
+++ database/migrations/2025_11_14_083017_create_tasks_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+
+            $table->string("name");
+            $table->text("description")->nullable();
+            $table->boolean("is_completed")->default(false);
+            $table->integer("position")->default(0);
+            $table->dateTime("deadline")->nullable();
+
+            $table->foreignId("creator_id")->constrained("users")->cascadeOnDelete();
+            $table->foreignId("assignee_id")->nullable()->constrained("users")->nullOnDelete();
+
+            $table->foreignIdFor(\App\Models\Column::class)->constrained()->cascadeOnDelete();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
